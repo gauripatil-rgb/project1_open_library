91c2ed3d9617d88708a6f470ea92927a1949fa68
Gauri Patil <gauri.patil@fafadiatech.com>
2025-11-12 15:19:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import click
+
+@click.command()
+@click.argument("title")
+@click.option('--count', default=6, help="number of books")
+
+def title_search(title, count):
+    """search book by its title."""
+
+    URL = f"https://openlibrary.org/search.json?title={title}"
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        data = response.json()
+        book = data.get("docs", [])
+        if not book:
+            print("No books found")
+            return
+        for books in book[:count]:
+            print("📘Title:", books.get("title"))
+            print("Author Name:", books["author_name"][0])
+            print("Author Key:", books["author_key"][0])
+            print("📅first publish year:", books["first_publish_year"],"\n")
+        click.echo(f"The book name has been fetched successfully by {title}")
+    else:
+        print("Error:", response.status_code)
+
+if __name__ == "__main__":
+    title_search()
+    
+
+
+
+
+
+
